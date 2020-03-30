306a8fcf5e211466126da172914992a3b123f216
ale-cci <alecrd98@gmail.com>
2020-03-30 21:18:12 +0200

    Add MST algorithm in masd

---
+++ masd/algorithms/floyd_warshall.py
@@ -0,0 +1,12 @@
+'''
+Floyd Warshall
+--------------
+
+algorythm used to find the shorted path in a weighted graph
+with positive and negative edges.
+
+Complexity: O(n^3)
+'''
+
+if __name__ == '__main__':
+    pass
